224cb632ab2e43c359c12c13edc3371c2b5785c2
fern <n8fern@gmail.com>
2013-04-16 11:25:05 -0700

    socket update

--- RoboHooker/Assets/CharacterScripts/SocketWeapon.cs
+++ RoboHooker/Assets/CharacterScripts/SocketWeapon.cs
@@ -2,6 +2,12 @@ using UnityEngine;
 using System.Collections;
 
 public class SocketWeapon : Weapon {
+
+    public void Equip(GameObject Character)
+    {
+        transform.parent=Character.transform;
+    }
+
     public KeyCode fireKey
     {
         get
