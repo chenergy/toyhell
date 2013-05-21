bb0ed57e964ce032afa5c97e45658e617af19c82
Fern <n8fern@gmail.com>
2013-05-21 07:01:41 -0700

    still working out the robot's reload

---
+++ RoboHooker/Assets/CharacterScripts/Cannon.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+using System.Collections;
+
+public class Cannon : Weapon
+{
+    public GameObject m_barbieHead;
+    public GameObject m_CannonLoc;
+    private bool m_loaded=true;
+    private GameObject m_firedHead;
+    public override void fire(Vector2 dir)
+    {
+        if (m_loaded)
+        {
+            if (dir.magnitude < .2)
+            {
+                dir.x = transform.forward.x;
+            }
+            m_loaded = false;
+            Debug.Log("dir is " + dir);
+            m_firedHead=(GameObject) Instantiate(m_barbieHead, 
+                m_CannonLoc.transform.position/*+transform.forward*2*/, transform.rotation);
+        }
+    }
+    void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log(collision.gameObject.name);
+        if (collision.gameObject == m_firedHead)
+        {
+            Destroy(collision.gameObject);
+            m_loaded = true;
+        }
+    }
+
+}
--- RoboHooker/Assets/CharacterScripts/Robot.cs
+++ RoboHooker/Assets/CharacterScripts/Robot.cs
@@ -3,14 +3,13 @@ using System.Collections;
 
 public class Robot : PlayerCharacter
 {
-
-    public GameObject m_RightWeapon;
+/*    public GameObject m_RightWeapon;
     public SocketWeapon m_RightScript;
     public KeyCode m_RightEuipKey;
-    private bool m_Loaded = true;
 
     public override void Update()
     {
         base.Update();
     }
+ */
 }
